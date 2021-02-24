36f8655403b34b8c64d8edfc1d8b113ae3d1622a
Jana Němcová <ja_nemcova@volny.cz>
2021-02-24 21:11:18 +0100

    fix missing var

--- src/parseTls.js
+++ src/parseTls.js
@@ -3,7 +3,7 @@ return avg_count of the aggregations
 */
 
 export default function parseTls(response) {
-  tls_cn = [];
+  var tls_cn = [];
   if (response && response.aggregations && response.aggregations.agg && response.aggregations.agg.buckets.length > 0) {
       tls_cn = response.aggregations.agg.buckets.map(key => key.key);
   }
