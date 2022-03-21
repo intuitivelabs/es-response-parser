a3c97750761949484480e692a9b86363827cefa9
Jana Němcová <ja_nemcova@volny.cz>
2022-03-21 20:04:29 +0100

    table pagination

--- src/parseTableHits.js
+++ src/parseTableHits.js
@@ -1,3 +1,13 @@
 export default function parseTableHits(hits) {
     return hits;
 }
+
+async function decryptTableHits(data, profile, length, page) {
+    return data;
+}
+
+
+export {
+    parseTableHits,
+    decryptTableHits
+  };
