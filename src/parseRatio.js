eb2d1437b2b5f5bb665171d0828c93d0f234787a
Jana Nemcova <jana.nemcova@frafos.com>
2022-11-28 11:09:24 +0100

    rename fnc

--- src/parseRatio.js
+++ src/parseRatio.js
@@ -2,7 +2,7 @@
 return field1/field2 ratio value
 */
 
-export default function parseAggAvgCnt(response) {
+export default function parseRatio(response) { 
   if (response && response.aggregations && response.aggregations.field1 && response.aggregations.field1.value && response.aggregations.field2 && response.aggregations.field2.value) {
     return Math.round(response.aggregations.field1.value/response.aggregations.field2.value);
   }
