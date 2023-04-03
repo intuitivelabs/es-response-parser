9a71b898cf1d55f5609c321b4a4867dd8c5d36b8
Jana Nemcova <jana.nemcova@frafos.com>
2023-04-03 12:40:24 +0200

    connectivity CA empty values fix

--- src/parseMultipleData.js
+++ src/parseMultipleData.js
@@ -15,18 +15,37 @@ export default function parseMultipleData(response) {
             if (dataParse[j].agg && dataParse[j].agg.value) {
                values.value0= dataParse[j].agg.value;
             }
+            else {
+                values.value0 = 0;
+            }
+
             if (dataParse[j].agg2 && dataParse[j].agg2.value) {
                 values.value1= dataParse[j].agg2.value;
+            }
+            else {
+                values.value1 = 0;
             }
             if (dataParse[j].agg3 && dataParse[j].agg3.value) {
                 values.value2= dataParse[j].agg3.value;
             }
+            else {
+                values.value2= 0;
+            }
+
             if (dataParse[j].agg4 && dataParse[j].agg4.value) {
                 values.value3= dataParse[j].agg4.value;
             }
+            else {
+                values.value3 = 0;
+            }
+
             if (dataParse[j].agg6 && dataParse[j].agg6.value) {
                 values.value4= dataParse[j].agg6.value / 100;
             }
+            else {
+                values.value4 = 0;
+            }
+
             dataFinal.push({
                 name: dataParse[j].key,
                 values: values
